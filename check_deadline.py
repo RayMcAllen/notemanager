6a280b9a29e2db1ea4ae8e470586b4d2058d0643
RayMcAllen <roma.zharyonov1996@gmail.com>
2025-01-18 13:51:55 +0300

    check_deadline.py

--- check_deadline.py
+++ check_deadline.py
@@ -1,32 +1,28 @@
 import datetime #импортируем библиотеку для работы с датами
 
 # функция для определения текущей даты
-def today():
-    date = datetime.datetime.today().strftime("%d.%m.%Y")
-    print("Сегодня:\n", date)
+
+date = datetime.datetime.today().strftime("%d.%m.%Y")
+print("Сегодня: ", date)
 
 # функция для обработки пользовательского ввода
 def issue_date():
     while True:
-        global create_date
-        create_date = input('Введите дату завершения дедлайна в формате ДД.ММ.ГГГГ:\n')
+        create_date = input('Введите дату завершения дедлайна в формате ДД.ММ.ГГГГ: ')
         try:
             create_date = datetime.datetime.strptime(create_date, "%d.%m.%Y")
         except:
             print("Дата введена некорректно!")
             continue
         print('Дата завершения дедлайна:\n', create_date.strftime("%d.%m.%Y"))
+        remain = create_date - datetime.datetime.now()
+        if remain.days > 1:
+            print("Осталось ", remain.days, 'дней')
+        elif remain.days < -1:
+            print("Дедлайн завершился", -remain.days, "дней назад!")
+        else:
+            print("Дедлайн сегодня")
         return
 
-# функция для расчета разницы между датами
-def deadline():
-    remain = create_date - datetime.datetime.now()
-    if remain.days > 1:
-        print("Осталось ", remain.days, 'дней')
-    elif remain.days < 1:
-        print("Дедлайн завершился", -remain.days, "дней назад!")
-
 #вызов созданных функций
-today()
 issue_date()
-deadline()
